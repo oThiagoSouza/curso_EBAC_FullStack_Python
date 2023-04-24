5fad9ec679209dec8c87a4b130cb917ee6d353d9
Thiago Souza <111827538+oThiagoSouza@users.noreply.github.com>
2023-04-24 03:01:54 -0300

    Add files via upload

---
+++ exercico_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplica(2, 4));
+function dizOla(nome) {
+    return "Olá, " + nome;
+}
+console.log(dizOla("João"));
---
+++ exercico_ts.ts
@@ -0,0 +1,11 @@
+function multiplica(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+console.log(multiplica(2, 4));
+
+function dizOla(nome: string): string {
+    return "Olá, " + nome;
+}
+
+console.log(dizOla("João"));
